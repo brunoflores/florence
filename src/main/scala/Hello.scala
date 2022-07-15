3f843a002035a9a98e54291b0cf99fe53a885acd
Bruno Flores <bruno@brunoflores.com.br>
2022-07-16 09:29:43 +1000

    Tweak the example.

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -12,24 +12,29 @@ import chisel3._
 
 class Hello extends Module {
   val io = IO(new Bundle {
-    val led = Output(UInt(1.W))
+    val led1 = Output(UInt(1.W))
+    val led2 = Output(UInt(1.W))
   })
-  val CNT_MAX = (50000000 / 2 - 1).U
+  val CNT_MAX = 12_000_000.U
 
   val cntReg = RegInit(0.U(32.W))
-  val blkReg = RegInit(0.U(1.W))
+  val blkReg1 = RegInit(0.U(1.W))
+  val blkReg2 = RegInit(1.U(1.W))
 
   cntReg := cntReg + 1.U
   when(cntReg === CNT_MAX) {
     cntReg := 0.U
-    blkReg := ~blkReg
+    blkReg1 := ~blkReg1
+    blkReg2 := blkReg1
   }
-  io.led := blkReg
+  io.led1 := blkReg1
+  io.led2 := blkReg2
 }
 
 /**
  * An object extending App to generate the Verilog code.
  */
 object Hello extends App {
-  (new chisel3.stage.ChiselStage).emitVerilog(new Hello())
+  (new chisel3.stage.ChiselStage).emitVerilog(
+    new Hello(), Array("--target-dir", "generated"))
 }
--- verilog/hello_top.v
+++ verilog/hello_top.v
@@ -1,10 +1,13 @@
-module hello_top(input clk, output led);
-  wire h_io_led;
-  wire res;
+module hello_top(input sysclk, output led1, output led2);
+  wire h_io_led1;
+  wire h_io_led2;
+  wire rst;
 
-  assign led = h_io_led;
-  assign res = 1'h0;
+  assign led1 = h_io_led1;
+  assign led2 = h_io_led2;
+  assign rst = 1'h0;
 
-  Hello h(.clock(clk), .reset(res),
-       .io_led( h_io_led ));
+  Hello h(.clock(sysclk), .reset(rst),
+       .io_led1( h_io_led1 ),
+       .io_led2( h_io_led2 ));
 endmodule
