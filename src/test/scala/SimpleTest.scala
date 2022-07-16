317baa457a4f0238c753bf7ddd3bc8cb785579e6
Bruno Flores <bruno@brunoflores.com.br>
2022-07-16 10:33:09 +1000

    Use scala.

--- src/test/scala/SimpleTest.scala
+++ src/test/scala/SimpleTest.scala
@@ -5,21 +5,13 @@ import org.scalatest.flatspec.AnyFlatSpec
 class SimpleTest extends AnyFlatSpec with ChiselScalatestTester {
   "DUT" should "pass" in {
     test(new Dut).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
-      dut.io.a.poke(0.U)
-      dut.io.b.poke(0.U)
-      dut.clock.step()
-
-      dut.io.a.poke(1.U)
-      dut.io.b.poke(0.U)
-      dut.clock.step()
-
-      dut.io.a.poke(0.U)
-      dut.io.b.poke(1.U)
-      dut.clock.step()
-
-      dut.io.a.poke(1.U)
-      dut.io.b.poke(1.U)
-      dut.clock.step()
+      for (a <- 0 until 4) {
+        for (b <- 0 until 4) {
+          dut.io.a.poke(a.U)
+          dut.io.b.poke(b.U)
+          dut.clock.step()
+        }
+      }
     }
   }
 }
