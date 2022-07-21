322aeaae0f15d3dad2132289cb88315f0f4ad55d
Bruno Flores <bruno@brunoflores.com.br>
2022-07-21 19:53:22 +1000

    Begin LC3.

---
+++ src/main/scala/lc3/Alu.scala
@@ -0,0 +1,32 @@
+package lc3
+
+import chisel3._
+
+object Alu {
+  val SZ_ALU_FN = 4
+  val XLEN = 32
+
+  def FN_ADD = 0.U
+
+  def isSub(cmd: UInt) = cmd(3)
+}
+
+import Alu._
+
+class Alu extends Module {
+  val io = IO(new Bundle {
+    val fn = Input(UInt(SZ_ALU_FN.W))
+    val in2 = Input(UInt(XLEN.W))
+    val in1 = Input(UInt(XLEN.W))
+    val out = Output(UInt(XLEN.W))
+    val adder_out = Output(UInt(XLEN.W))
+  })
+
+  // ADD, SUB
+  val in2_inv = Mux(isSub(io.fn), ~io.in2, io.in2)
+  io.adder_out := io.in1 + in2_inv + isSub(io.fn)
+
+  val out = Mux(io.fn === FN_ADD, io.adder_out, 0.U)
+
+  io.out := out
+}
---
+++ src/test/scala/lc3/AluTest.scala
@@ -0,0 +1,33 @@
+package lc3
+
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class AluTest extends AnyFlatSpec with ChiselScalatestTester {
+  behavior of "ALU"
+  it should "add" in {
+    test(new Alu).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.io.fn.poke(0.U)
+      dut.io.in1.poke(40.U)
+      dut.io.in2.poke(2.U)
+      dut.io.out.expect(42.U)
+    }
+  }
+  it should "add large numbers" in {
+    test(new Alu).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.io.fn.poke(0.U)
+      dut.io.in1.poke((scala.math.pow(2, 32) - 2).toLong.U)
+      dut.io.in2.poke(1.U)
+      dut.io.out.expect((scala.math.pow(2, 32) - 1).toLong.U)
+    }
+  }
+  it should "wrap around" in {
+    test(new Alu).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      dut.io.fn.poke(0.U)
+      dut.io.in1.poke((scala.math.pow(2, 32) - 1).toLong.U)
+      dut.io.in2.poke(2.U)
+      dut.io.out.expect(1.U)
+    }
+  }
+}
